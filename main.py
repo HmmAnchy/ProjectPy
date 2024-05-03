baf0e921c32086b7a7d29d73564fba061f952c68
kab12 <email.com>
2024-05-03 11:38:17 +0300

    Добавлена иконка

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600)) # размер окна 
+pygame.display.set_caption("Название игры") # Название программы
+icon = pygame.image.load('images/icon.png') # фото иконки
+pygame.display.set_icon(icon) # иконка
+
+
 
 running = True
-
 while running:
 
 	pygame.display.update()
